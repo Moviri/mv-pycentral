c3a23015b60eb66fc04ee898c3327b02428fe29f
Karthik Satheesh Kumar <49326615+KarthikSKumar98@users.noreply.github.com>
2025-11-03 13:11:52 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as fh:
     long_description = fh.read()
 
-VERSION = "2.0a9"
+VERSION = "2.0a10"
 
 setuptools.setup(
     name="pycentral",
