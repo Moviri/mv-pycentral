1d641e4eab38d6438e2c80024dc6a6497125ac64
Karthik Satheesh Kumar <karthik.satheesh-kumar@hpe.com>
2026-02-26 14:35:47 -0800

    Bump version to 2.0a14 and update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as fh:
     long_description = fh.read()
 
-VERSION = "2.0a13"
+VERSION = "2.0a14"
 
 setuptools.setup(
     name="pycentral",
@@ -15,11 +15,7 @@ setuptools.setup(
     description="HPE Aruba Networking Central Python Package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    project_urls={
-        "Documentation": "https://pycentral.readthedocs.io/en/v2/",
-        "Repository": "https://github.com/aruba/pycentral/",
-        "Issues": "https://github.com/aruba/pycentral/issues",
-    },
+    url="https://github.com/aruba/pycentral",
     packages=setuptools.find_packages(exclude=["docs", "tests", "sample_scripts"]),
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
@@ -31,12 +27,12 @@ setuptools.setup(
     ],
     python_requires=">=3.8",
     install_requires=[
-        "requests==2.32.4",
-        "PyYAML==6.0.2",
+        "requests==2.32.5",
+        "PyYAML==6.0.3",
         "oauthlib==3.2.2",
         "requests_oauthlib==2.0.0",
         "pytz==2025.2",
-        "protobuf==6.33.2",
+        "protobuf==6.33.5",
         "websocket-client==1.9.0",
     ],
     extras_require={"colorLog": ["colorlog"]},
